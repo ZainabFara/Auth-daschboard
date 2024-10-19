a864e5249bd214753c33cf3040e0fb81a6d8600d
ZainabFara <zainabfarah050@gmail.com>
2024-10-19 10:47:03 +0200

    la till användarschema och autentiseringslogik

---
+++ backend/models/user.model.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    name: {
+        type: String,
+        require: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verifictionToken: String,
+    verificationsTokenexpiresAt: Date,
+},{timestamps: true});
+
+// Fälten "created" och "updated" kommer automatiskt att läggas till i dokumentet
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,17 +1,12 @@
+//Den här filen fungerar som en router för dina autentiseringsrutter
 import express from "express";
+import { login, signup , logout } from "../controllers/auth.controllers.js";
+
 
 const router = express.Router();
 
-router.get("/signup",(req, res) => {
-    res.send("signup route");
-});
-
-router.get("/login",(req, res) => {
-    res.send("login route");
-});
-
-router.get("/logout",(req, res) => {
-    res.send("logout route");
-});
+router.get("/signup", signup);
+router.get("/login",login);
+router.get("/logout",logout);
 
 export default router;
