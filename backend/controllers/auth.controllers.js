052d0277eec81e43e2efa55096c6f67a845f5d56
ZainabFara <zainabfarah050@gmail.com>
2024-10-20 20:00:01 +0200

    Hashed password

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,7 @@
 //Här har jag ontrollerfunktioner för att hantera mina rutter.
 import { User } from '../models/user.model.js';
+import bcrypt from 'bcryptjs';
+
 export const signup = async (req, res) => {
    const {email, password, name} = req.body;
 
@@ -12,7 +14,21 @@ export const signup = async (req, res) => {
       if(userAlreadyExists) {
          return res.status(400).json({success:false, message: "User already exists"});
       } 
-      
+
+      const hashedPassword = await bcrypt.hash(password,10);
+      const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+      const user = new User ({
+         email,
+         password: hashedPassword,
+         name,
+         verificationToken,
+         verificationTokenExiresAt: Date.now() + 24 * 60 * 60 * 1000 // 24 timmar 
+      })
+
+      await user.save();
+
+      //jwt
+      generateTokenAndSetCookie(res.user._id)
 
    } catch (error) {
      res.status(400).json({sucess:false, meddage: error.message})
